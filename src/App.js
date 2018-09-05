06289dfae0f497589f7b6c95945c3467ee6ff66e
Mehrdad <your_email@youremail.comyour_email@youremail.com>
2018-09-05 11:45:03 +0430

    add files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+      render() {
+        
+        return(
+            <div className="App">
+                
+                        <p className="red">   <p className="box">Box1</p></p>
+                        <p className="green"> <p className="box">Box2</p></p>
+                        <p className="blue">  <p className="box">Box3</p></p>
+                 
+            </div>
+        )
+    }
+}
+
+export default App;
